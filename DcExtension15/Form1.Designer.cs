86ba9e08ef8cb8c38811ba45db580a21d2d39250
Jeronimo Fermin Deiros <jeronimo.deiros@globant.com>
2020-10-28 13:50:30 -0300

    initial commit

---
+++ DcExtension15/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace DcExtension15
+{
+    partial class frmDcExtension
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDcExtension));
+            this.gbSwitch = new System.Windows.Forms.GroupBox();
+            this.btnSwitch = new System.Windows.Forms.Button();
+            this.wbDCNews = new System.Windows.Forms.WebBrowser();
+            this.gbNews = new System.Windows.Forms.GroupBox();
+            this.niIcono = new System.Windows.Forms.NotifyIcon(this.components);
+            this.mnuContextual = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.activarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.desactivarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblYourVersion = new System.Windows.Forms.Label();
+            this.lbDevelopedBy = new System.Windows.Forms.Label();
+            this.lblNumberOfVersion = new System.Windows.Forms.Label();
+            this.gbSwitch.SuspendLayout();
+            this.gbNews.SuspendLayout();
+            this.mnuContextual.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbSwitch
+            // 
+            this.gbSwitch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbSwitch.AutoSize = true;
+            this.gbSwitch.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.gbSwitch.Controls.Add(this.btnSwitch);
+            this.gbSwitch.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.gbSwitch.Location = new System.Drawing.Point(9, 6);
+            this.gbSwitch.Name = "gbSwitch";
+            this.gbSwitch.Size = new System.Drawing.Size(950, 219);
+            this.gbSwitch.TabIndex = 2;
+            this.gbSwitch.TabStop = false;
+            this.gbSwitch.Text = "DC EXTENSION SWITCH";
+            // 
+            // btnSwitch
+            // 
+            this.btnSwitch.AutoSize = true;
+            this.btnSwitch.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnSwitch.BackColor = System.Drawing.Color.Black;
+            this.btnSwitch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSwitch.Location = new System.Drawing.Point(3, 16);
+            this.btnSwitch.Name = "btnSwitch";
+            this.btnSwitch.Size = new System.Drawing.Size(944, 200);
+            this.btnSwitch.TabIndex = 0;
+            this.btnSwitch.UseVisualStyleBackColor = false;
+            this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
+            this.btnSwitch.MouseLeave += new System.EventHandler(this.btnSwitch_MouseLeave);
+            this.btnSwitch.MouseHover += new System.EventHandler(this.btnSwitch_MouseHover);
+            // 
+            // wbDCNews
+            // 
+            this.wbDCNews.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wbDCNews.Location = new System.Drawing.Point(3, 16);
+            this.wbDCNews.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbDCNews.Name = "wbDCNews";
+            this.wbDCNews.Size = new System.Drawing.Size(944, 247);
+            this.wbDCNews.TabIndex = 0;
+            this.wbDCNews.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // gbNews
+            // 
+            this.gbNews.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbNews.AutoSize = true;
+            this.gbNews.Controls.Add(this.wbDCNews);
+            this.gbNews.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.gbNews.Location = new System.Drawing.Point(9, 274);
+            this.gbNews.Name = "gbNews";
+            this.gbNews.Size = new System.Drawing.Size(950, 266);
+            this.gbNews.TabIndex = 3;
+            this.gbNews.TabStop = false;
+            this.gbNews.Text = "NEWS";
+            // 
+            // niIcono
+            // 
+            this.niIcono.Icon = ((System.Drawing.Icon)(resources.GetObject("niIcono.Icon")));
+            this.niIcono.Text = "DeathCommandExtension";
+            this.niIcono.Visible = true;
+            this.niIcono.Click += new System.EventHandler(this.niIcono_Click);
+            this.niIcono.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.niIcono_MouseDoubleClick);
+            // 
+            // mnuContextual
+            // 
+            this.mnuContextual.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.activarToolStripMenuItem,
+            this.desactivarToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.mnuContextual.Name = "mnuContextual";
+            this.mnuContextual.Size = new System.Drawing.Size(102, 70);
+            // 
+            // activarToolStripMenuItem
+            // 
+            this.activarToolStripMenuItem.Name = "activarToolStripMenuItem";
+            this.activarToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.activarToolStripMenuItem.Text = "ON";
+            this.activarToolStripMenuItem.Click += new System.EventHandler(this.activarToolStripMenuItem_Click);
+            // 
+            // desactivarToolStripMenuItem
+            // 
+            this.desactivarToolStripMenuItem.Name = "desactivarToolStripMenuItem";
+            this.desactivarToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.desactivarToolStripMenuItem.Text = "OFF";
+            this.desactivarToolStripMenuItem.Click += new System.EventHandler(this.desactivarToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.salirToolStripMenuItem.Text = "QUIT";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // lblYourVersion
+            // 
+            this.lblYourVersion.AutoSize = true;
+            this.lblYourVersion.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYourVersion.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.lblYourVersion.Location = new System.Drawing.Point(30, 237);
+            this.lblYourVersion.Name = "lblYourVersion";
+            this.lblYourVersion.Size = new System.Drawing.Size(208, 30);
+            this.lblYourVersion.TabIndex = 4;
+            this.lblYourVersion.Text = "YOUR VERSION: ";
+            // 
+            // lbDevelopedBy
+            // 
+            this.lbDevelopedBy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbDevelopedBy.AutoSize = true;
+            this.lbDevelopedBy.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDevelopedBy.ForeColor = System.Drawing.Color.Lime;
+            this.lbDevelopedBy.Location = new System.Drawing.Point(738, 246);
+            this.lbDevelopedBy.Name = "lbDevelopedBy";
+            this.lbDevelopedBy.Size = new System.Drawing.Size(221, 18);
+            this.lbDevelopedBy.TabIndex = 5;
+            this.lbDevelopedBy.Text = "Developed by: JD - Developer";
+            // 
+            // lblNumberOfVersion
+            // 
+            this.lblNumberOfVersion.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblNumberOfVersion.AutoSize = true;
+            this.lblNumberOfVersion.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumberOfVersion.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.lblNumberOfVersion.Location = new System.Drawing.Point(298, 237);
+            this.lblNumberOfVersion.Name = "lblNumberOfVersion";
+            this.lblNumberOfVersion.Size = new System.Drawing.Size(0, 30);
+            this.lblNumberOfVersion.TabIndex = 6;
+            // 
+            // frmDcExtension
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(971, 552);
+            this.Controls.Add(this.lblNumberOfVersion);
+            this.Controls.Add(this.lbDevelopedBy);
+            this.Controls.Add(this.lblYourVersion);
+            this.Controls.Add(this.gbNews);
+            this.Controls.Add(this.gbSwitch);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(987, 550);
+            this.Name = "frmDcExtension";
+            this.Text = "DC Extension 2015";
+            this.Load += new System.EventHandler(this.frmDcExtension_Load);
+            this.gbSwitch.ResumeLayout(false);
+            this.gbSwitch.PerformLayout();
+            this.gbNews.ResumeLayout(false);
+            this.mnuContextual.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbSwitch;
+        private System.Windows.Forms.WebBrowser wbDCNews;
+        private System.Windows.Forms.GroupBox gbNews;
+        private System.Windows.Forms.Button btnSwitch;
+        private System.Windows.Forms.NotifyIcon niIcono;
+        private System.Windows.Forms.ContextMenuStrip mnuContextual;
+        private System.Windows.Forms.ToolStripMenuItem activarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem desactivarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.Label lblYourVersion;
+        private System.Windows.Forms.Label lbDevelopedBy;
+        private System.Windows.Forms.Label lblNumberOfVersion;
+    }
+}
+
---
+++ DcExtension15/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace DcExtension15
+{
+    public partial class frmDcExtension : Form
+    {
+
+        #region Atributos
+
+        Image on = DcExtension15.Properties.Resources.ON;
+        Image off = DcExtension15.Properties.Resources.OFF;
+
+        Image Switch_Fifa15_On = DcExtension15.Properties.Resources.switch_fifa15_ON;
+        Image Switch_Fifa15_Off = DcExtension15.Properties.Resources.switch_fifa15_OFF;
+        Image Switch_Fifa15_intermedio = DcExtension15.Properties.Resources.switch_fifa15_intermedio;
+
+        //obtengo el path del directorio donde se encuentra el .exe
+        string DirectorioRaiz = Directory.GetCurrentDirectory();
+        //nombre de la carpeta original del juego
+        string DataORIGINAL = Directory.GetCurrentDirectory() + @"\data";
+        string DlcORIGINAL = Directory.GetCurrentDirectory() + @"\dlc";
+        //nombre de la carpeta DC Extension
+        string DataDC =Directory.GetCurrentDirectory() + @"\data_dc";
+        string DlcDC =Directory.GetCurrentDirectory() + @"\dlc_dc";
+        //nombre de la carpeta backup de la original
+        string DataBKP =Directory.GetCurrentDirectory() + @"\data_bkp";
+        string DlcBKP =Directory.GetCurrentDirectory() + @"\dlc_bkp";
+        
+        bool DCEActivado = false;
+        bool Regenerar = false;
+
+        public class Constante
+        {
+            public const string ACTIVADO = "DCExtension ON";
+            public const string DESACTIVADO = "DCExtension OFF";
+        }
+        #endregion
+
+        #region Eventos
+
+        public frmDcExtension()
+        {
+
+            InitializeComponent();
+
+            CheckVersion();
+
+            if (DCExtensionActivated())
+            {
+                DCEActivado = true;
+                btnSwitch.Image = Switch_Fifa15_On;
+                IconoDeNotificacionActivado();
+            }
+            else
+            {
+                DCEActivado = false;
+                btnSwitch.Image = Switch_Fifa15_Off;
+                IconoDeNotificacionDesactivado();
+            }
+            //Si existe la carpeta DATA_BKP es porque estaba activado de una instancia anterior
+            //entonces lo desactivamos, salvo la primera vez:  
+            if (DCExtensionActivated() && Regenerar)
+                DesactivarDCExtension();
+            else
+                if(Regenerar)
+                    ActivarDCExtension();
+
+            Regenerar = true;
+                        
+        }
+     
+        private void frmDcExtension_Load(object sender, EventArgs e)
+        {        
+            ActivarWebBrowserDCNews();
+        }
+
+    
+
+        private void btnSwitch_Click(object sender, EventArgs e)
+        {
+            SwitchDCExtension();
+        }
+
+        private void activarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (DCEActivado)
+                MessageBox.Show("Death Command Extension already set.");
+            else
+                SwitchDCExtension();
+        }
+
+        private void desactivarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!DCEActivado)
+                MessageBox.Show("Death Command Extension already disabled.");
+            else
+                SwitchDCExtension();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void niIcono_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            SwitchDCExtension();
+        }
+
+        private void btnSwitch_MouseHover(object sender, EventArgs e)
+        {
+            btnSwitch.Image = Switch_Fifa15_intermedio;
+        }
+
+        private void btnSwitch_MouseLeave(object sender, EventArgs e)
+        {
+            if (DCEActivado)
+                btnSwitch.Image = Switch_Fifa15_On;
+            else
+                btnSwitch.Image = Switch_Fifa15_Off;
+        }
+
+        //click izquierdo en el icono de notificacion muestra el menu contextual.
+        private void niIcono_Click(object sender, EventArgs e)
+        {
+            mnuContextual.Show(Cursor.Position);
+        }
+        #endregion
+
+
+
+        #region Metodos
+
+        bool DCExtensionActivated()
+        {
+            return Directory.Exists(DataBKP);
+        }
+
+        private void CheckVersion()
+        {
+            if (File.Exists(DirectorioRaiz + @"\dcVersion.txt"))
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader(DirectorioRaiz + @"\dcVersion.txt");
+                string line;
+                line = file.ReadLine();
+                lblNumberOfVersion.Text = line;
+            }
+            else
+            {
+                lblNumberOfVersion.Text = "UNKNOWN";
+            }
+
+        }
+        private void ActivarDCExtension()
+        {           
+            IntercambioDirectoriosADC();
+            DCEActivado = true;
+            btnSwitch.Image = Switch_Fifa15_On;
+            IconoDeNotificacionActivado();
+            RegenerarArchivosSubProceso();           
+        }
+
+        private void IntercambioDirectoriosADC()
+        {
+            try
+            {               
+                System.IO.Directory.Move(DataORIGINAL, DataBKP);
+                System.IO.Directory.Move(DlcORIGINAL, DlcBKP);
+
+                System.IO.Directory.Move(DataDC, DataORIGINAL);
+                System.IO.Directory.Move(DlcDC, DlcORIGINAL);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw ex;
+            }        
+        }
+
+        private void DesactivarDCExtension()
+        {            
+            IntercambioDirectoriosAOrig();
+            DCEActivado = false;
+            btnSwitch.Image = Switch_Fifa15_Off;
+            IconoDeNotificacionDesactivado();
+            RegenerarArchivosSubProceso();
+           
+        }
+
+        private void RegenerarArchivosSubProceso()
+        {
+            Process p = new Process();
+            ProcessStartInfo psi = new ProcessStartInfo("cmd.exe");
+            psi.Arguments = "/c \"regenerator.exe -key -care -all";
+            p.StartInfo = psi;
+            p.Start();
+        }
+        private void IntercambioDirectoriosAOrig()
+        {
+            try
+            {
+
+                System.IO.Directory.Move(DataORIGINAL, DataDC);
+                System.IO.Directory.Move(DlcORIGINAL, DlcDC);
+
+                System.IO.Directory.Move(DataBKP, DataORIGINAL);
+                System.IO.Directory.Move(DlcBKP, DlcORIGINAL);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show( ex.Message);
+                throw ex;
+            }      
+        }
+        private void IconoDeNotificacionActivado()
+        {
+            niIcono.Icon = DcExtension15.Properties.Resources.ON_ico;
+            niIcono.ContextMenuStrip = this.mnuContextual;
+            niIcono.ShowBalloonTip(1, "ON", "Death Command extension has been activated", ToolTipIcon.Info);
+            niIcono.Text = Constante.ACTIVADO;
+        }     
+             
+
+        private void SwitchDCExtension()
+        {
+            if (DCEActivado)
+            {
+                DesactivarDCExtension();
+            }
+            else
+            {
+                ActivarDCExtension();
+            }
+        }
+
+     
+
+        private void IconoDeNotificacionDesactivado()
+        {
+            niIcono.Icon = DcExtension15.Properties.Resources.OFF_ico;
+            niIcono.ContextMenuStrip = this.mnuContextual;
+            niIcono.ShowBalloonTip(1, "OFF", "Death Command extension has been disabled", ToolTipIcon.Info);
+            niIcono.Text = Constante.DESACTIVADO;
+        }
+
+        
+        
+        
+        private void ActivarWebBrowserDCNews()
+        {
+            // seteo la direccion de las news y navego a la pagina.
+            string urlNews = "http://www.deathcommand.com/latest-extension-fifa15_update/";
+           
+            wbDCNews.ScriptErrorsSuppressed = true;
+            wbDCNews.Navigate(new Uri(urlNews));
+        }
+
+
+
+
+        #endregion
+
+
+
+
+
+        #region NewsDCExtension   
+        //para manejo de errores en la webBox (no lo estoy usando)
+        //al final use esto: wbDCNews.ScriptErrorsSuppressed = true;
+
+
+        // Hides script errors without hiding other dialog boxes.
+        private void SuppressScriptErrorsOnly(WebBrowser browser)
+        {
+            // Ensure that ScriptErrorsSuppressed is set to false.
+            browser.ScriptErrorsSuppressed = false;
+
+            // Handle DocumentCompleted to gain access to the Document object.
+            browser.DocumentCompleted +=
+                new WebBrowserDocumentCompletedEventHandler(
+                    browser_DocumentCompleted);
+        }
+
+        private void browser_DocumentCompleted(object sender,
+            WebBrowserDocumentCompletedEventArgs e)
+        {
+            ((WebBrowser)sender).Document.Window.Error +=
+                new HtmlElementErrorEventHandler(Window_Error);
+        }
+
+        private void Window_Error(object sender,
+            HtmlElementErrorEventArgs e)
+        {
+            // Ignore the error and suppress the error dialog box. 
+            e.Handled = true;
+        }
+
+        #endregion
+
+        
+
+    }
+}
